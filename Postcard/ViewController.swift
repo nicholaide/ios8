a07448619a52c93c0b0983fcff514a74bfeaddbe
Nicholai de Guzman <nicholaideguzman@nis-imac.dhcp.swmed.org>
2014-12-29 16:15:49 -0600

    adding text via code

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //ui elem stor  cariable name    type !optionals
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +25,10 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        enterMessageTextField.text=""
+    }
 }
 
